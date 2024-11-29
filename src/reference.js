9d01694754e8383766f2b53c196598156cef458a
Julien Veyssier <julien-nc@posteo.net>
2024-11-29 14:49:16 +0100

    remove webpack-specific stuff
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/reference.js
+++ src/reference.js
@@ -1,12 +1,8 @@
 import { registerWidget, registerCustomPickerElement, NcCustomPickerRenderResult } from '@nextcloud/vue/dist/Components/NcRichText.js'
-import { getRequestToken } from '@nextcloud/auth'
-
-__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
-__webpack_public_path__ = OC.linkTo('contacts', 'js/') // eslint-disable-line
 
 registerWidget('users_picker_profile', async (el, { richObjectType, richObject, accessible }) => {
-	const { default: Vue } = await import(/* webpackChunkName: "reference-issue-lazy" */'vue')
-	const { default: ProfilePickerReferenceWidget } = await import(/* webpackChunkName: "reference-issue-lazy" */'./components/ProfilePicker/ProfilePickerReferenceWidget.vue')
+	const { default: Vue } = await import('vue')
+	const { default: ProfilePickerReferenceWidget } = await import('./components/ProfilePicker/ProfilePickerReferenceWidget.vue')
 	Vue.mixin({ methods: { t, n } })
 	const Widget = Vue.extend(ProfilePickerReferenceWidget)
 	new Widget({
@@ -21,7 +17,7 @@ registerWidget('users_picker_profile', async (el, { richObjectType, richObject, 
 registerCustomPickerElement('profile_picker', async (el, { providerId, accessible }) => {
 	const { default: Vue } = await import(/* webpackChunkName: "vue-lazy" */'vue')
 	Vue.mixin({ methods: { t, n } })
-	const { default: ProfilesCustomPicker } = await import(/* webpackChunkName: "image-picker-lazy" */'./components/ProfilePicker/ProfilesCustomPicker.vue')
+	const { default: ProfilesCustomPicker } = await import('./components/ProfilePicker/ProfilesCustomPicker.vue')
 	const Element = Vue.extend(ProfilesCustomPicker)
 	const vueElement = new Element({
 		propsData: {
